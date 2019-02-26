6eb69e36cc224dd6c82cf75259d6fa6ec235b6d3
JohnPolson <JP@JPs-MacBook-Pro.local>
2019-02-26 00:11:36 +0000

    MVP

---
+++ src/models/prime_checker.js
@@ -0,0 +1,32 @@
+const PubSub = require("../helpers/pub_sub.js");
+
+const PrimeChecker = function () {
+
+};
+
+PrimeChecker.prototype.bindEvents = function () {
+  PubSub.subscribe("FormView:number-input", (event) => {
+    const checkedNumber = this.numberIsPrime(event.detail);
+    let result = undefined;
+      if (checkedNumber === true){
+        result = `Congratulations - that's a prime number.`
+      } else {
+        result = `Sorry - that's not a prime number.`
+      }
+    PubSub.publish("PrimeChecker:numberChecked", result);
+  })
+};
+
+PrimeChecker.prototype.numberIsPrime = function (number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+        return false;
+    }
+  }
+  return true;
+};
+
+module.exports = PrimeChecker;
